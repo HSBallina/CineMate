8700a539f0cb92290b5f167f7ad96542adcf31c4
HSBallina <hakan@badpixel.se>
2025-11-17 23:07:37 +0100

    chore: add api settings

---
+++ code/CineMate/CineMate/Configuration/ApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CineMate.Configuration;
+
+public class ApiSettings
+{
+    public string OpenAiEndpoint { get; set; }
+    public string OpenAiKey { get; set; }
+    public string TmdbApiKey { get; set; }
+}
--- code/CineMate/CineMate/Program.cs
+++ code/CineMate/CineMate/Program.cs
@@ -1,15 +1,24 @@
+using CineMate.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder
+    .Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddUserSecrets("05e6dfde-36ac-4746-b349-584137f8e836");
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
+ApiSettings apiSettings = new();
+builder.Configuration.GetSection("ApiSettings").Bind(apiSettings);
+builder.Services.AddSingleton(apiSettings);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
