530f6bd2593f9f01bc3b3ce0824d7cb28aa8223d
HSBallina <hakan@badpixel.se>
2025-11-18 11:55:10 +0100

    refcator: add props to tmdbmovie

--- code/CineMate/CineMate/Models/TmdbMovie.cs
+++ code/CineMate/CineMate/Models/TmdbMovie.cs
@@ -2,7 +2,12 @@
 
 public class TmdbMovie
 {
-    public string Title { get; set; }
-    public string PosterPath { get; set; }
+    public required string Title { get; set; }
+    public required string PosterPath { get; set; }
+    public required string ImdbId { get; set; }
+    public required string Overview { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+    public required string Tagline { get; set; }
     public double VoteAverage { get; set; }
+    public required List<string> Genres { get; set; }
 }
